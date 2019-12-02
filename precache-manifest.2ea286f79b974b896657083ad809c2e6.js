26bb3c78f00970be102a4358c72bc44cf6a05012
Rohit Garg <rohit@unity3d.com>
2019-12-01 19:28:42 -0800

    First version of the website!

---
+++ precache-manifest.2ea286f79b974b896657083ad809c2e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af079347a210420f645700b63def777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d59eb01624ef6ce299",
+    "url": "/static/css/main.0f0c26ad.chunk.css"
+  },
+  {
+    "revision": "0ba55e767b111ce9626c",
+    "url": "/static/js/2.f212af72.chunk.js"
+  },
+  {
+    "revision": "59d59eb01624ef6ce299",
+    "url": "/static/js/main.6073e878.chunk.js"
+  },
+  {
+    "revision": "a380bcfac60e95035a7b",
+    "url": "/static/js/runtime-main.72957b6f.js"
+  },
+  {
+    "revision": "6ff7d4801d256a2b5d6da3f313935d9c",
+    "url": "/static/media/cover_resized.6ff7d480.jpg"
+  },
+  {
+    "revision": "ea5c383d43021ab9de14d801d39551ce",
+    "url": "/static/media/story_full.ea5c383d.jpg"
+  }
+]);
